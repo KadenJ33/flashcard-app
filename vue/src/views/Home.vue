49a474f8b120f6000237198f18316b9fcf481b05
Kaden Rankin <toastmystrudle33@gmail.com>
2020-12-15 19:51:20 -0500

    deleted white-space

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,28 +1,21 @@
 <template>
-
   <div class="home">
-  
     <view-decks />
-    
-    <!-- <router-view :key="$route.fullPath" class="routerView" /> -->
+    <!--<router-view :key="$route.fullPath" class="routerView" /> -->
     <!-- <div class="button">
-      
       <button class="edit" @click="$router.push('edit')">Edit Deck</button>
       <button class="delete" @click="$router.push('delete')">Delete Deck</button>
       </div> -->
   </div>
-
 </template>
 
 <script>
 import ViewDecks from '@/views/ViewDecks.vue';
 
-
 export default {
   name: "home",
   components: {
     ViewDecks,
-    
  }
 };
 </script>
